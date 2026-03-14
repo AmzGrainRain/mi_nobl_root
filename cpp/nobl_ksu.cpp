734ca8c39e6b38179a6f0912220821197a9c69c2
AmzGrainRain <amzgrainrain@outlook.com>
2026-03-14 16:40:29 +0800

    feat(cpp): add skip functionality and improve error handling in nobl_ksu

--- cpp/nobl_ksu.cpp
+++ cpp/nobl_ksu.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 #include <cstdio>
 #include <numeric>
+#include <cstdint>
 
 #define NOMINMAX
 #include <Windows.h>
@@ -19,10 +20,11 @@ const auto cwd = fs::current_path();
 auto adb_bin = cwd / "adb" / "adb.exe";
 auto fastboot_bin = cwd / "adb" / "fastboot.exe";
 auto ksum = cwd / "ksu.apk";
-auto ksud = cwd / "ksud";
+
+uint16_t skip_flag = 0;
 
 [[maybe_unused]]
-static auto Exec(const std::string& bin, const std::string& args) -> std::tuple<int, std::string>
+static auto Exec(const std::string& bin, const std::string& args, const bool print_error = true) -> std::tuple<int, std::string>
 {
 	std::string cmd = std::format("{} {} 2>&1", bin, args);
 	FILE* pipe = _popen(cmd.c_str(), "r");
@@ -49,6 +51,11 @@ static auto Exec(const std::string& bin, const std::string& args) -> std::tuple<
 	}
 
 	int exit_code = _pclose(pipe);
+	if (exit_code != 0 && print_error)
+	{
+		std::println("❌ 命令执行失败！错误信息：{}", output);
+	}
+
 	return { exit_code, output };
 }
 
@@ -57,7 +64,15 @@ static auto Step1() -> bool
 	std::cout <<
 		"========================================\n"
 		"   第一步：准备进入 Fastboot 模式\n"
-		"========================================\n"
+		"========================================\n";
+
+	if (skip_flag & 0b0001)
+	{
+		std::println("✅ 跳过此步骤...");
+		return true;
+	}
+
+	std::cout <<
 		"📱 请完成以下操作：\n"
 		"  1.打开开发者模式（设置 - 关于手机 - 连续点击版本号）\n"
 		"  2.解锁手机并进入系统桌面\n"
@@ -68,19 +83,24 @@ static auto Step1() -> bool
 	std::getline(std::cin, tmp); (void)tmp;
 
 	std::println("🔍 等待设备连接...");
-	const auto r1 = Exec(adb_bin.string(), "devices");
-	if (std::get<0>(r1) != 0)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r1));
-		return false;
+		const auto [code, res] = Exec(adb_bin.string(), "devices");
+		if (code != 0)
+		{
+			std::println("❌ 命令执行失败！错误信息：{}", res);
+			return false;
+		}
 	}
 
-	std::println("🔄 正在重启设备进入 Fastboot 模式...");
-	const auto r2 = Exec(adb_bin.string(), "reboot bootloader");
-	if (std::get<0>(r2) != 0)
+
+	std::println("🔄 正在重启到 Fastboot 模式...");
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r2));
-		return false;
+		const auto [code, res] = Exec(adb_bin.string(), "reboot bootloader");
+		if (code != 0)
+		{
+			std::println("❌ 命令执行失败！错误信息：{}", res);
+			return false;
+		}
 	}
 
 	return true;
@@ -91,27 +111,24 @@ static auto Step2() -> bool
 	std::cout <<
 		"\n========================================\n"
 		"   第二步：宽容 SELinux\n"
-		"========================================\n"
-		"📱 请确认设备已进入 Fastboot 模式（屏幕显示 Fastboot 字样）\n"
-		"✅ 确认后按回车键继续...";
+		"========================================\n";
 
-	std::string tmp;
-	std::getline(std::cin, tmp); (void)tmp;
-
-	std::println("🚀 正在通过漏洞设置 SELinux 为 Permissive...");
-	const auto r1 = Exec(fastboot_bin.string(), "oem set-gpu-preemption 0 androidboot.selinux=permissive");
-	if (std::get<0>(r1) != 0)
+	if (skip_flag & 0b0010)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r1));
-		return false;
+		std::println("✅ 跳过此步骤...");
+		return true;
+	}
+
+	std::println("🚀 将 SELinux 设置为宽容模式...");
+	{
+		const auto [code, res] = Exec(fastboot_bin.string(), "oem set-gpu-preemption 0 androidboot.selinux=permissive");
+		if (code != 0) return false;
 	}
 
 	std::println("🚀 正在引导设备进入系统...");
-	const auto r2 = Exec(fastboot_bin.string(), "continue");
-	if (std::get<0>(r2) != 0)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r2));
-		return false;
+		const auto [code, res] = Exec(fastboot_bin.string(), "continue");
+		if (code != 0) return false;
 	}
 
 	return true;
@@ -122,28 +139,26 @@ static auto Step3() -> bool
 	std::cout <<
 		"\n========================================\n"
 		"   第三步：检查 SELinux 状态\n"
-		"========================================\n"
-		"📱 请确认设备已开机\n"
-		"✅ 确认后按回车键继续...";
+		"========================================\n";
 
-	std::string tmp;
-	std::getline(std::cin, tmp); (void)tmp;
+	if (skip_flag & 0b0100)
+	{
+		std::println("✅ 跳过此步骤...");
+		return true;
+	}
 
 	std::println("🔍 等待设备连接...");
 	Exec(adb_bin.string(), "wait-for-device");
 
 	std::println("🔍 检查 SELinux 状态...");
-	const auto r1 = Exec(adb_bin.string(), "shell getenforce");
-	if (std::get<0>(r1) != 0)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r1));
-		return false;
-	}
-
-	if (std::get<1>(r1).find("Permissive") == std::string::npos)
-	{
-		std::println("❌ SELinux 仍处于 Enforcing 模式，可能是漏洞利用失败了！");
-		return false;
+		const auto [code, res] = Exec(adb_bin.string(), "shell getenforce");
+		if (code != 0) return false;
+		if (res.find("Permissive") == std::string::npos)
+		{
+			std::println("❌ SELinux 仍处于 Enforcing 模式，可能是漏洞利用失败了！");
+			return false;
+		}
 	}
 
 	std::println("✅ SELinux 已成功设置为 Permissive 模式！");
@@ -155,10 +170,17 @@ static auto Step4() -> bool
 	std::cout <<
 		"\n========================================\n"
 		"   第四步：安装 KernelSU\n"
-		"========================================\n"
+		"========================================\n";
+
+	if (skip_flag & 0b1000)
+	{
+		std::println("✅ 跳过此步骤...");
+		return true;
+	}
+
+	std::cout <<
 		"📱 请打开锁屏并进入桌面\n"
 		"✅ 确认后按回车键继续...";
-
 	std::string tmp;
 	std::getline(std::cin, tmp); (void)tmp;
 
@@ -166,58 +188,46 @@ static auto Step4() -> bool
 	Exec(adb_bin.string(), "wait-for-device");
 
 	std::println("🔍 检查 KernelSU Manager 的安装情况...");
-	const auto r5 = Exec(adb_bin.string(), "shell pm path me.weishu.kernelsu");
-	if (std::get<0>(r5) == 0) // KernelSU Manager 已安装会返回 0 并且输出路径，否则会返回 1
 	{
-		std::println("✅ 已安装 KernelSU Manager，路径：{}", std::get<1>(r5));
-		std::println("🛠️ 是否需要重新安装 KernelSU Manager？");
-		std::println("📝 输入 Y 或 y 后按回车即代表重新安装");
-		std::println("📝 输入其他任意内容或直接按回车即代表跳过");
-		std::print("您的选择: ");
-		std::string choice;
-		std::getline(std::cin, choice); (void)choice;
-		if (choice.empty() || (choice[0] != 'y' && choice[0] != 'Y'))
+		const auto [code, _] = Exec(adb_bin.string(), "shell pm path me.weishu.kernelsu", false);
+		if (code == 0) // KernelSU Manager 已安装会返回 0 并且输出路径，否则会返回 1
 		{
-			std::println("✅ 跳过安装...");
-			return true;
-		}
+			std::println("\n\n✅ 检测到您已安装 KernelSU Manager，是否需要重新安装？");
+			std::println("📝 输入 Y 或 y 后按回车即代表重新安装");
+			std::println("📝 输入其他任意内容或直接按回车即代表跳过");
+			std::print("您的选择: ");
+			std::string choice;
+			std::getline(std::cin, choice); (void)choice;
+			if (choice.empty() || (choice[0] != 'y' && choice[0] != 'Y'))
+			{
+				std::println("✅ 跳过安装...");
+				return true;
+			}
 
-		std::println("🚀 正在卸载 KernelSU Manager...");
-		const auto r6 = Exec(adb_bin.string(), "shell pm uninstall me.weishu.kernelsu");
-		if (std::get<0>(r6) != 0)
-		{
-			std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r6));
-			return false;
+			std::println("🚀 正在卸载 KernelSU Manager...");
+			const auto [code, res] = Exec(adb_bin.string(), "shell pm uninstall me.weishu.kernelsu");
+			if (code != 0) return false;
+			std::println("✅ 已成功卸载 KernelSU Manager！");
+
 		}
-		std::println("✅ 已成功卸载 KernelSU Manager！");
 	}
 
 	std::println("📦 推送 KernelSU Manager 到 /data/local/tmp/ksu.apk");
-	const auto r7 = Exec(adb_bin.string(), std::format("push {} /data/local/tmp/ksu.apk", ksum.string()));
-	if (std::get<0>(r7) != 0)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r7));
-		return false;
+		const auto [code, res] = Exec(adb_bin.string(), std::format("push {} /data/local/tmp/ksu.apk", ksum.string()));
+		if (code != 0) return false;
 	}
 
-	std::println("🛠️ 接下来请留意手机上的安装提示");
-	std::println("✅ 确认后按回车键继续...");
+
+	std::cout <<
+		"🛠️ 接下来请留意手机上的安装提示\n"
+		"✅ 确认后按回车键继续...";
 	std::getline(std::cin, tmp); (void)tmp;
 
 	std::println("🚀 安装 KernelSU Manager...");
-	const auto r8 = Exec(adb_bin.string(), "shell pm install -r /data/local/tmp/ksu.apk");
-	if (std::get<0>(r8) != 0)
 	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r8));
-		return false;
-	}
-
-	std::println("🚀 一些清理工作...");
-	const auto r9 = Exec(adb_bin.string(), "shell rm /data/local/tmp/ksu.apk");
-	if (std::get<0>(r9) != 0)
-	{
-		std::println("❌ 命令执行失败！错误信息：{}", std::get<1>(r9));
-		return false;
+		const auto [code, res] = Exec(adb_bin.string(), "shell pm install -r /data/local/tmp/ksu.apk");
+		if (code != 0) return false;
 	}
 
 	return true;
@@ -229,42 +239,57 @@ auto main() -> int
 		SetConsoleOutputCP(CP_UTF8);
 
 		std::println("===================================================================\n\n");
-		std::println("   ✨ 项目地址 https://github.com/AmzGrainRain/mi_nobl_ksu\n");
+		std::println("   ✨ 项目地址 https://github.com/AmzGrainRain/nobl_ksu\n");
 		std::println("   📝 工作目录：{}\n\n", cwd.string());
 		std::println("===================================================================\n\n");
 
 		if (!fs::exists(adb_bin))
 		{
-			std::println("当前目录下没有 adb/adb.exe");
-			return 1;
+			throw std::runtime_error("当前目录下没有 adb/adb.exe");
 		}
 		adb_bin = adb_bin.lexically_relative(cwd);
 
 		if (!fs::exists(fastboot_bin))
 		{
-			std::println("当前目录下没有 adb/fastboot.exe");
-			return 1;
+			throw std::runtime_error("当前目录下没有 adb/fastboot.exe");
 		}
 		fastboot_bin = fastboot_bin.lexically_relative(cwd);
 
-		if (!fs::exists(ksud))
-		{
-			std::println("当前目录下没有 ksud");
-			return 1;
-		}
-		ksud = ksud.lexically_relative(cwd);
-
 		if (!fs::exists(ksum))
 		{
-			std::println("当前目录下没有 ksu.apk");
-			return 1;
+			throw std::runtime_error("当前目录下没有 ksu.apk");
 		}
 		ksum = ksum.lexically_relative(cwd);
+
+		std::println("🚀 一些准备工作...");
+		Exec(adb_bin.string(), "kill-server", false);
+
+		std::println("🔍 检查设备是否处于 Fastboot 模式...");
+		{
+			const auto [code, res] = Exec(fastboot_bin.string(), "devices");
+			if (code == 0 && !res.empty())
+			{
+				skip_flag |= 0b0001; // 跳过步骤 1
+			}
+		}
+
+		std::println("🔍 检查设备 SELinux 状态...");
+		{
+			const auto [code, res] = Exec(adb_bin.string(), "shell getenforce");
+			if (code == 0 && res.find("Permissive") != std::string::npos)
+			{
+				skip_flag |= 0b0111; // 跳过步骤 1 2 3
+			}
+		}
 
 		if (!Step1()) throw std::runtime_error("step 1 error");
 		if (!Step2()) throw std::runtime_error("step 2 error");
 		if (!Step3()) throw std::runtime_error("step 3 error");
 		if (!Step4()) throw std::runtime_error("step 4 error");
+
+		std::println("🚀 一些清理工作...");
+		Exec(adb_bin.string(), "shell rm /data/local/tmp/ksu.apk", false);
+		Exec(adb_bin.string(), "kill-server", false);
 
 		std::cout <<
 			"\n========================================\n"
@@ -275,11 +300,12 @@ auto main() -> int
 	catch (const std::exception& ex)
 	{
 		std::println("程序发生异常：{}", ex.what());
+		std::this_thread::sleep_for(5s);
+		system("pause");
 		return 1;
 	}
 
 	std::this_thread::sleep_for(5s);
-
 	system("pause");
 	return 0;
 }
